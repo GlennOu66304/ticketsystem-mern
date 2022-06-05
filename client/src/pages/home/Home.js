f707d4253f62012e360791399f37d5e3d98c1e2b
Glenn <glenou@yahoo.com>
2022-06-05 18:24:37 +0800

    homePage

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,10 +1,22 @@
 import React from "react";
-
+import { FaQuestionCircle, FaEnvelope } from "react-icons/fa";
 import "./home.css";
 
 export default function Home() {
-  return <div className="homeContainer">
+  return (
+    <div className="homeContainer">
+      <h1>What do yu need help with</h1>
+      <p>Please choose from an option below</p>
 
-    This is the Home page
-  </div>;
+      <div className="create-tickets">
+        <FaQuestionCircle />
+        <span>create New Ticket</span>
+      </div>
+
+      <div className="check-tickets">
+        <FaEnvelope />
+        <span> View My tickets</span>
+      </div>
+    </div>
+  );
 }
